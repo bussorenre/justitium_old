732211b02d3d4e65a2d78debfaf795776b70a30d
Ryo Matsumoto <ryo.mats302@gmail.com>
2014-07-08 11:59:57 +0900

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+/**********************************************************************
+
+  main.c - online judge system
+
+  $Author$
+  created at: Tue Jul  8 11:58:53 JST 2014
+
+  Copyright (C) 2014-2015 Ryo Matsumoto
+
+**********************************************************************/
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
